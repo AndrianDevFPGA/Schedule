7e62b85a59b90bdb1118e555d7fb77b279581362
Rakotojaona Nambinina <104864333+AndrianDevFPGA@users.noreply.github.com>
2023-04-16 18:34:11 +0200

    Update schedule.v

--- schedule.v
+++ schedule.v
@@ -40,3 +40,34 @@ module schedule (
     end 
   
 endmodule
+
+/*
+entity schedule is
+  port (
+    clk, rst : in std_logic;
+    scheduleEntry : in std_logic_vector (31 downto 0);
+    tx : out std_logic
+  );
+end schedule;
+
+architecture rtl of schedule is
+  signal gtb : std_logic_vector (31 downto 0);
+begin
+  process (clk) is 
+  begin
+    if (rst = '1') then
+      gtb <= (others =>'0');
+      tx <= '0';
+    elsif rising_edge(clk) then
+      gtb <= gtb + 1;
+      if (gtb = scheduleEntry) then
+        gtb <= (others => '0');
+        tx <= '1';
+      else
+        tx <= '0';
+      end if;
+    end if;
+  end process;
+end rtl;
+
+*/
